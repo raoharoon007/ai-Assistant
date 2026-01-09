bd175db052eab8f810f7faa1c22d92e078cb1770
raoharoon007 <raoharoon007@gmail.com>
2026-01-09 09:48:46 +0500

    update vercel.json for routing rewrites and reorder plugins in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tailwindcss()],
+  plugins: [ react(), tailwindcss(),svgr()],
 })
 
 
